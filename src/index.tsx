7848906753d1b9cf9646bad72bd86e25f58f8a0b
Ishanka Dilshan <ishankadilshan64@gmail.com>
2025-03-09 13:07:01 +0530

    commit 3

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Import your Publishable Key from the environment
-const PUBLISHABLE_KEY = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY
-
-// Throw an error if the Publishable Key is missing
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
-}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
